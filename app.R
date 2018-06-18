e0db0b4e2ad3938b68a0b20832fb2204688c3584
wim <wfjvdham@gmail.com>
2018-06-18 11:11:09 +0200

    added check on empty imput data, changed filename

--- app.R
+++ app.R
@@ -8,8 +8,8 @@ ui <- fluidPage(
   titlePanel("Extract your Kobo Notes"),
   sidebarLayout(
     sidebarPanel(
-      fileInput("file_input", "Choose *.annot File to extract your notes from",
-                accept = c("text/xml")),
+      fileInput("file_input", "Choose *.annot file to extract your notes from",
+                accept = ".annot"),
       uiOutput("download_button"),
       a("Maracuya IT", href="http://maracuya-it.com/")
     ),
@@ -29,7 +29,6 @@ server <- function(input, output) {
     
     tryCatch(
       {
-        print(input$file_input)
         rv$notes <- input$file_input$datapath %>%
           read_html() %>%
           html_nodes("fragment") %>%
@@ -40,12 +39,16 @@ server <- function(input, output) {
         stop(safeError(e))
       }
     )
-    rv$notes
+    if (length(rv$notes) > 0) {
+      rv$notes
+    } else {
+      "Cannot extract any notes from the file. Are you sure it is a valid *.annot file?"
+    }
   })
   
   output$downloadData <- downloadHandler(
     filename = function() {
-      paste0(input$file_input$name, "_notes.txt")
+      paste0(input$file_input$name, ".notes.txt")
     },
     content = function(con) {
       rv$notes %>%
@@ -55,7 +58,9 @@ server <- function(input, output) {
   
   output$download_button <- renderUI({
     req(input$file_input) 
-    downloadButton("downloadData", label = "Download Notes", class="btn btn-primary")
+    if (length(rv$notes) > 0) {
+      downloadButton("downloadData", label = "Download Notes", class="btn btn-primary")
+    }
   })
 }
 
